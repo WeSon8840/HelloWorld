2cdc20f3ece7c3f1fbdfe192535149393de9a5cb
WeSon8840 <WeSon8840@7HJL6Y1.Student.UGDSB.ED>
2017-02-10 13:52:41 -0500

    First change for HelloWorld.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author WeSon8840
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // CODE GOES HERE
+        System.out.print("Hello World");
+        System.out.println("Hi");
+    }
+    
+}
